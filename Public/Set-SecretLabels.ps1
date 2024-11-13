e5fdad1587fc9e080a58bf1b195bc28c08f3cb93
Peter Qwärnström <peter.qwarnstrom@gmail.com>
2024-11-13 12:32:59 +0100

    Set-SecretLabels help

--- Public/Set-SecretLabels.ps1
+++ Public/Set-SecretLabels.ps1
@@ -1,3 +1,19 @@
+<#
+.SYNOPSIS
+    Add labels on an Aptitud secret
+.DESCRIPTION
+    This function will add labels to an existing secret
+.PARAMETER SecretName
+    The name of the secret
+.PARAMETER Labels
+    A key value pair list of labels to add to the secret
+    Only hyphens (-), underscores (_), lowercase characters, and numbers are allowed
+.EXAMPLE
+    PS> > Set-SecretLabels -Secretname "MySecret" -Labels "type=application"
+.EXAMPLE
+    PS> > Set-SecretLabels -Secretname "MySecret" -Labels "type=application,format=json"
+#>
+
 function Set-SecretLabels {
   [CmdletBinding()]
   param (
